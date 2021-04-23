d012126deb213f3892622bc97d2f69e218a7b616
xyzhub <3479304511@qq.com>
2021-04-23 20:42:34 +0800

    头像渲染功能完成

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -1,4 +1,10 @@
 $.ajaxPrefilter(function(option) {
     // console.log(option);
     option.url = 'http://api-breakingnews-web.itheima.net' + option.url;
+    // 判断是否需要请求头
+    if (option.url.indexOf('/my/') !== -1) {
+        option.headers = {
+            Authorization: localStorage.getItem('token') || ''
+        }
+    }
 })
---
+++ assets/js/index.js
@@ -0,0 +1,53 @@
+// 首页入口函数
+$(function() {
+    // 调用用户数据函数
+    getUserInfo();
+    // 定义获取用户数据
+    function getUserInfo() {
+        // 调用接口
+        $.ajax({
+            type: "GET",
+            url: "/my/userinfo",
+            success: function(res) {
+                console.log(res);
+                // 判断是否获取成功
+                if (res.status !== 0) {
+                    return layui.lay.msg(res.message, { icon: 5 })
+                }
+                // 获取成功调用渲染头像函数
+                renderAvatar(res.data);
+            }
+        });
+    }
+
+    // 定义渲染头像函数
+    function renderAvatar(data) {
+        // console.log(data);
+        // 优先获取用户的昵称,其次用户名
+        var name = data.nickname || data.username;
+        // 渲染欢迎语
+        $('#welcome').html('欢迎&nbsp;&nbsp;' + name);
+        // 渲染头像区域
+        // 判断是否有头像
+        if (data.user_pic !== null) {
+            // 有图片头像
+            // 显示图片头像
+            $('.layui-nav-img').attr('src', data.user_pic).show();
+            // 隐藏文本头像
+            $('.text-avatar').hide();
+        } else {
+            // 无图片头像
+            // 获取用户名的首字母
+            var firstCode = name[0].toUpperCase();
+            // 显示文本头像
+            $('.text-avatar').html(firstCode);
+            // 隐藏图片头像
+            $('.layui-nav-img').hide();
+
+        }
+
+    }
+
+
+
+})
